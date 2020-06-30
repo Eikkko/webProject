d7595f825dc3b34c319065d2c5f3441620e7b981
Eikkko <804781685@qq.com>
2020-07-01 03:36:11 +0800

    7.1

--- ks/js/index.js
+++ ks/js/index.js
@@ -24,22 +24,57 @@ $(window).scroll(function(){
 });
 
 
- //当滚动条的位置处于距顶部500像素以下时，跳转链接出现，否则消失
+ //当滚动条的位置处于距顶部500像素以下时，返回顶部出现，否则消失
  $(function () {
      $(window).scroll(function(){
 		var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
 		var backTop = $("#star").offset().top - $(window).height();
+		var navTop = $("#nav").offset().top;
+
          if(scrollT >= 500){
-         		 $("#totop").css('display','block');
+         	 $("#totop").css('display','block');
+				 
          }
          if(scrollT < 500){
          	 $("#totop").css('display','none');
          }
+	 //当滚动条的位置处于距顶部30像素以下时，吸顶灯出现，否则消失
+		if(scrollT >= 30){
+		 	 $("#nav").css({
+		 	 "position":"fixed",
+		 	 "left":"0",
+		 	 "right":"0",
+		 	 "top":"0",
+		 	 "margin":"0 auto",
+		 	 "zIndex":"1000"
+		 	  }); 
+		 }
+		if(scrollT < 30){
+		 	 $("#nav").css({
+			 "position":"static",
+			  });
+		 }
      });
-     //当点击跳转链接后，回到页面顶部位置
+     //当点击返回顶部后，回到页面顶部位置
      $("#btn").click(function(){
          $('body,html').animate({scrollTop:0},1000);
          return false;
      });
+	 if(scrollT >= 30){
+	  		 	 $("#nav").css({
+	  		 		"position":"fixed",
+	  		 		"left":"0",
+	  		 		"right":"0",
+	  		 		"top":"0",
+	  		 		"margin":"0 auto",
+	  		 		"zIndex":"1000"
+	  		 		 }); 
+	  		 }
+	  		 if(scrollT < 30){
+	  		 	$("#nav").css({
+	 			"position":"static",
+	  		 });
+	 }
  });
  
+
